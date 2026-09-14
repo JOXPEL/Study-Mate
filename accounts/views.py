954a7a6ff6abe429f300e209ed735416da2edfeb
JOXPEL <youssefafify788@gmail.com>
2026-09-15 00:25:06 +0300

    feat: implement auth views, forms, and basic dashboard layout.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registeration successfully!")
+            return redirect('dashboard:home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard:home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from quizzes.models import QuizAttempt
+from .models import StudyPlan
 
-# Create your views here.
+@login_required
+def dashboard_home(request):
+    attempts = QuizAttempt.objects.filter(student=request.user).order_by('-completed_at')[:5]
+    plans = StudyPlan.objects.filter(student=request.user)
+    context = {
+        'attempts': attempts,
+        'plans': plans
+    }
+    return render(request, 'dashboard/home.html', context)
